0fcfd2853776ca47572a8588c45731ede47385f3
Melissa Brewer <melramirez727@gmail.com>
2021-07-13 23:02:25 -0500

    prompt questions working in terminal

--- index.js
+++ index.js
@@ -58,7 +58,17 @@ const questions = [
       message: 'What is your email?',
   },
   ];
+  // TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {
+  inquirer.prompt(questions)
   
+}
+
+// Function call to initialize app
+init();
 
 
 
